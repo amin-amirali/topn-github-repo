4f26593db6a7bcecbb79ca1e740343bf9beb38ae
Amin Amirali <amin.amirali@telenordigital.com>
2019-10-23 17:25:58 +0200

    Complete first prototype

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "3.10.0"]
-                 [yogthos/config "1.1.6"]]
+                 [yogthos/config "1.1.6"]
+                 [peco "0.1.6"]]
   :test-paths ["test"]
   :main topn-github-repo.core
   :jvm-opts ["-Dconfig=config/config.edn"]
--- src/topn_github_repo/core.clj
+++ src/topn_github_repo/core.clj
@@ -1,28 +1,48 @@
 (ns topn-github-repo.core
   (:require [clj-http.client :as client]
             [config.core :refer [env]]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [clojure.string :as str]
+            [peco.core :refer [tokenizer]])
   (:gen-class))
 
 (def endpoint "https://api.github.com/")
+(def list-type "issues")
+
 (def url (str
            endpoint
            "repos/"
            (:git-owner env) "/"
            (:git-repo env) "/"
-           "issues"))
+           list-type))
+
+(def tokenize (tokenizer [:lower-case :remove-numbers :porter-stem :remove-stop-words]))
+
+(defn tokenize-string [str-header]
+  (map #(tokenize %) (str/split str-header #" ")))
+
+(defn stem-data [list]
+  (flatten (map #(tokenize-string %) list)))
+
+(defn filter-columns
+  [all-issues]
+  (map #(:title %) all-issues))
 
 (defn get-all-issues
   [url]
-  (-> (client/get url {:as :json :query-params {"state" "all"
-                                                "sort" "created"
-                                                "direction" "desc"
-                                                "page" "1"}})
+  (-> (client/get url {:as :json :query-params {"state" (:state env)
+                                                "sort" (:sort-by env)
+                                                "direction" (:sort-direction env)
+                                                ;"page" "10000"
+                                                ;"per_page" "1"
+                                                }})
       :body
-      (json/read-str :key-fn keyword)
-      first
-      :title))
+      (json/read-str :key-fn keyword)))
 
 (defn -main [& args]
-  (println url)
-  (get-all-issues url))
+  (->> (get-all-issues url)
+       filter-columns
+       stem-data
+       frequencies
+       (sort-by second >)
+       (take 10)))
